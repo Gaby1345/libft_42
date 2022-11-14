05802180ce3fd1a5d9b8bb5b93f49a6c49e7f255
gtrabajo <gtrabajo@student.42madrid.com>
2022-11-14 20:19:31 +0100

    Por favor funciona

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gtrabajo <gtrabajo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:57:32 by gtrabajo          #+#    #+#             */
-/*   Updated: 2022/11/14 20:11:52 by gtrabajo         ###   ########.fr       */
+/*   Updated: 2022/11/14 20:18:51 by gtrabajo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,44 @@ void	ft_putnbr_fd(int n, int fd)
 		write(fd, str, ft_strlen(str));
 		free(str);
 	}
+}
+
+
+/**************************************/
+void	printing(int n);
+
+void	ft_putnbr(int nb)
+{
+	int	recursiven;
+
+	if (nb < 0 && nb != -2147483648)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+		ft_putnbr(nb);
+		return ;
+	}
+	if (nb >= 0 && nb <= 9)
+		printing(nb);
+	else if (nb == -2147483648)
+	{
+		write (1, "-", 1);
+		write (1, "2147483648", 10);
+	}
+	else
+	{
+		recursiven = nb % 10;
+		ft_putnbr((nb - recursiven) / 10);
+		printing(recursiven);
+	}
+	return ;
+}
+
+void	printing(int n)
+{
+	char	c;
+
+	c = n + '0';
+	write(1, &c, 1);
+	return ;
 }
