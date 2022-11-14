ef971326224e3282f342bec7e9d86a0a45dfeb39
gtrabajo <gtrabajo@student.42madrid.com>
2022-11-14 20:13:53 +0100

    Acabando parte 2

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gtrabajo <gtrabajo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 19:57:32 by gtrabajo          #+#    #+#             */
-/*   Updated: 2022/11/14 20:11:52 by gtrabajo         ###   ########.fr       */
+/*   Created: 2022/11/14 19:53:04 by gtrabajo          #+#    #+#             */
+/*   Updated: 2022/11/14 19:56:57 by gtrabajo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-	char	*str;
+	char	c;
 
-	str = ft_itoa(n);
-	if (str != NULL)
-	{
-		write(fd, str, ft_strlen(str));
-		free(str);
-	}
+	c = '\n';
+	write(fd, s, ft_strlen(s));
+	write(fd, &c, 1);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gtrabajo <gtrabajo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 18:01:01 by gtrabajo          #+#    #+#             */
-/*   Updated: 2022/11/14 19:47:33 by gtrabajo         ###   ########.fr       */
+/*   Updated: 2022/11/14 20:00:02 by gtrabajo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
