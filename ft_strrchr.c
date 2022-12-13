93ffffb28370f573ceb1662aba1f2de1be661d33
Gaby1345 <60926120+Gaby1345@users.noreply.github.com>
2022-12-13 18:48:41 +0100

    no se que esta pasando
    errores tester

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gtrabajo <gtrabajo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabymb <gabymb@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 16:14:56 by gabymb            #+#    #+#             */
-/*   Updated: 2022/09/22 18:30:49 by gtrabajo         ###   ########.fr       */
+/*   Updated: 2022/12/13 18:40:21 by gabymb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		i;
-	char	*last;
+	int				len;
+	unsigned char	*auxs;
 
-	i = 0;
-	last = NULL;
-	while (s[i])
+	auxs = (unsigned char *)s;
+	len = ft_strlen((const char *)auxs);
+	while (len >= 0)
 	{
-		if (s[i] == (unsigned char)c)
-			last = ((char *)s + i);
-		i++;
+		if (*(auxs + len) == (unsigned char)c)
+			return ((char *)(auxs + len));
+		len--;
 	}
-	if (s[i] == (unsigned char)c)
-		return ((char *)s + i);
-	return (last);
+	return (NULL);
 }
